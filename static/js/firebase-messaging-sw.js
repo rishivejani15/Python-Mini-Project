e91b70608daa0e5edda968a202c3cb45f9035f6e
samyakchheda <127883494+samyakchheda@users.noreply.github.com>
2024-04-23 12:45:07 +0530

    Update firebase-messaging-sw.js

--- static/js/firebase-messaging-sw.js
+++ static/js/firebase-messaging-sw.js
@@ -3,13 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCKqR9wTb5eFBoQ8hULyaqFjaggDHMe1uM",
-authDomain: "quizapp-e74fe.firebaseapp.com",
-projectId: "quizapp-e74fe",
-storageBucket: "quizapp-e74fe.appspot.com",
-messagingSenderId: "1098236969903",
-appId: "1:1098236969903:web:814566823b75fe2557a55a",
-measurementId: "G-31DZJS4FLZ"
+   YOUR_FIREBASE_PROJECT_CONFIGURATION
 };
 
 // Initialize Firebase
